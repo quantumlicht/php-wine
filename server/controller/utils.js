7b3e441fcace2521d3699e99ddf1ca6e5ef87369
quantumlicht <guay.philippe@gmail.com>
2012-12-09 21:38:35 -0500

    form validation for subscription revamp

--- server/controller/utils.js
+++ server/controller/utils.js
@@ -1,13 +1,19 @@
 function isSamePassword (form) {
    var bRes = false;
    if(form.typePass.value !=form.retypePass.value){
-      $('#errorPass').text("mot de passe n''est pas le même").show();
-     document.getElementById('invalidPass').style.display='block'; 
+      $('#control-group-repass,#control-group-pass').addClass('error');
+      $('form#subscription button[type=submit]').attr('disabled','disabled');
+      $('<span/>',{
+         id:'pass',
+         class:'help-block',
+         text:"Les mots de passes saisis ne sont pas identiques."    
+      }).appendTo('#control-group-pass > div.controls');
    }
    else{
       bRes = true;
-       $('#errorPass').hide();
-      document.getElementById('invalidPass').style.display='none'; 
+      $('#control-group-repass,#control-group-pass').removeClass('error');
+      $('form#subscription button[type=submit]').removeAttr('disabled');
+      $('span#pass').remove();
    }
 
    return bRes;
@@ -17,13 +23,19 @@ function isValidEmail (form){
    var bRes = false;
    var validEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
    if (!validEmail.test(form.email.value)){
-      $('#errorEmail').text("Format d'adresse invalide.").show();
-      document.getElementById('invalidEmail').style.display='block'; 
+      $('#control-group-courriel').addClass('error');
+      $('form#subscription button[type=submit]').attr('disabled','disabled');
+      $('<span/>',{
+         id:'courriel',
+         class:'help-block',
+         text:"Le courriel n'est pas valide."    
+      }).appendTo('#control-group-courriel > div.controls');
    }
    else{
       bRes = true;
-      $('#errorEmail').hide();
-      document.getElementById('invalidEmail').style.display='none'; 
+      $('#control-group-courriel').removeClass('error');
+      $('form#subscription button[type=submit]').removeAttr('disabled');
+      $('span#courriel').remove();
    }
    return bRes;
 
@@ -33,13 +45,19 @@ function isValidUsername (form){
    var bRes=false;
    var invalidName = /[\s]/;
    if (invalidName.test(form.username.value)){
-      $('#errorUser').text("Pas d'espace permis.").show();
-      document.getElementById('invalidUsername').style.display='block'; 
+      $('#control-group-username').addClass('error');
+      $('form#subscription button[type=submit]').attr('disabled','disabled');
+      $('<span/>',{
+         id: 'username',
+         class:'help-block',
+         text:"Ce nom d'utilisateur n'est pas valide."    
+      }).appendTo('#control-group-username > div.controls');
    }
    else{
       bRes = true;
-      $('#errorUser').hide();
-      document.getElementById('invalidUsername').style.display='none';   
+      $('#control-group-username').removeClass('error');
+      $('form#subscription button[type=submit]').removeAttr('disabled');
+      $('span#username').remove();
    }
    return bRes;
 }
@@ -47,6 +65,19 @@ function isValidUsername (form){
 function ScrollBottom (){
    var container = $('#scrollContainer');
    container.scrollTop(container.prop('scrollHeight')); 
+}
+
+function getTextWidth(text){
+   var org = $(this)
+   var html = $('<span style="postion:absolute;width:auto;left:-9999px">' + (text || org.html()) + '</span>');
+   if (!text) {
+      html.css("font-family", org.css("font-family"));
+      html.css("font-size", org.css("font-size"));
+   }
+   $('body').append(html);
+   var width = html.width();
+   html.remove();
+   return width;
 }
 
 
